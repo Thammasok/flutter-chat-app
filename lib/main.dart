6f47b41abf62c07995ceb47cc5e11ace514b55ee
Jaranchai <jaranchai.nt@hotmail.com>
2020-03-04 18:42:29 +0700

    add firebase and message lists

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,6 @@ class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark().copyWith(
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.black54),
-        ),
-      ),
       initialRoute: WelcomeScreen.screenName,
       routes: <String, WidgetBuilder>{
         WelcomeScreen.screenName: (BuildContext context) => WelcomeScreen(),
